78dc843aeeaea6f0bf1f9e0dc097f1f9695b508b
bab7607@rit.edu <Brianna.Buttaccio@ad.eagledream.com>
2018-04-19 18:14:41 -0400

    demo passing in data from home to login and passing data back to change the displayed controller

--- src/app/home/home.component.js
+++ src/app/home/home.component.js
@@ -2,9 +2,10 @@ const HomeComponent = {
     template: require('./home.template.html'),
     controllerAs: 'vm',
     controller: class HomeComponent {
-      constructor(HomeService) {
+      constructor(HomeService, $scope) {
         'ngInject';
         this.HomeService = HomeService;
+        this.$scope = $scope;
   
       }
   
@@ -12,7 +13,23 @@ const HomeComponent = {
         this.HomeService.hello();
         const vm = this;
         vm.state = 'login';
+
+        vm.user = {
+          name: "John",
+          password: "123",
+          loggedIn: false
+        }
+  
+        /**
+         * true if the user has permission to edit all events OR if they have permission to edit their own event and this is an event they created
+         */
+        this.$scope.$watch('vm.user', function () {
+            if(vm.user.loggedIn == true){
+                vm.state = 'topic';
+            }
+        });
       }
+
     }
 };
 
--- src/app/login/login.component.js
+++ src/app/login/login.component.js
@@ -1,4 +1,15 @@
 const LoginComponent = {
+  bindings: {
+    /*
+    {
+      name: "John",
+      password: "123",
+      loggedIn: false
+    }
+    */
+    user: '=',
+  },
+  controllerAs: 'vm',
   template: require('./login.template.html'),
   controller: class LoginComponent {
     constructor(LoginService) {
@@ -9,6 +20,18 @@ const LoginComponent = {
 
     $onInit() {
       this.LoginService.hello();
+
+      const vm = this;
+
+      vm.initUserName = vm.user.name;
+
+      vm.goToTips = function(){
+        vm.user = {
+          name: "John",
+          password: "123",
+          loggedIn: true
+        }
+      }
     }
   }
 };

c9b75397c7590617ad6560f23f19b88da3bd1c28
Matt Atwell <ma3505@rit.edu>
2018-04-20 14:55:39 -0400

    Add php test folder + database

--- src/app/tips/tips.component.spec.js
+++ src/app/tips/tips.component.spec.js
@@ -4,16 +4,37 @@ import AppModule from '../app';
 
 describe('tips component controller', () => {
   let $controller;
+  let $service;
+  var allTopicTips = [
+  {
+    accountid: '1',
+    language: 'java',
+    description: 'Java Object Oriented Programming',
+    rating: 5
+  },
+  {
+    accountid: '5',
+    language: 'java',
+    description: 'Java Inheritance',
+    rating: 3
+  }
+
+];
 
   beforeEach(() => {
-    angular.mock.module(AppModule);
 
+    // angular.mock allows us to specify the module
+    angular.mock.module(AppModule);
+    // Inject AppModule Into Test
     angular.mock.inject((_$componentController_) => {
       $controller = _$componentController_('tips');
     });
+
   });
 
-  it('should pass this dumby test', () => {
+  it('should pass this du', () => {
     expect(true).toBeTruthy();
   });
+
+
 });
--- src/app/tips/tips.service.js
+++ src/app/tips/tips.service.js
@@ -5,7 +5,21 @@ export default class TipsService {
     this.$log = $log;
   }
 
+
   hello() {
     console.log('This is the tips component!');
   }
+
+  // Retrieves All the Tips for a particular topic from our PHP backend
+  getAllTips(topic){
+    console.log('This is the tips component!');
+  }
+
+  // retrieves tips created by the currently logged in user
+  getUserTips(login){
+
+  }
+
+
+
 }

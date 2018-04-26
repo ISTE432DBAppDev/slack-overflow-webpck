230519fbdecddcab9a81a60f14a5cb768128f4f7
Matt Atwell <ma3505@rit.edu>
2018-04-25 22:05:39 -0400

    format database script with serials

--- src/DataLayer/slackOverflowPostgre.sql
+++ src/DataLayer/slackOverflowPostgre.sql
@@ -5,7 +5,7 @@ CREATE DATABASE slackoverflow;
 
 DROP TABLE IF EXISTS ACCOUNTS CASCADE;
 CREATE TABLE ACCOUNTS (
-  accountID INT          NOT NULL,
+  accountID SERIAL       NOT NULL,
   username  VARCHAR(45)  NOT NULL,
   password  VARCHAR(256) NULL,
   salt      VARCHAR(64)  NOT NULL,
@@ -15,14 +15,14 @@ CREATE TABLE ACCOUNTS (
 
 DROP TABLE IF EXISTS LANGUAGE;
 CREATE TABLE LANGUAGE (
-  languageID INT NOT NULL,
+  languageID SERIAL NOT NULL,
   language   VARCHAR(45),
   PRIMARY KEY (languageID)
 );
 
 DROP TABLE IF EXISTS TIPS;
 CREATE TABLE TIPS (
-  tipsID      INT NOT NULL,
+  tipsID      SERIAL NOT NULL,
   accountID   INT NOT NULL REFERENCES ACCOUNTS (accountID),
   languageID  INT NOT NULL REFERENCES LANGUAGE (languageID),
   description VARCHAR(255),
@@ -39,42 +39,35 @@ CREATE TABLE USERRATINGS (
 
 INSERT INTO public.accounts (
   accountid, username, password, salt)
-VALUES (1, 'bri@gmail.com', '4c5f32a0f26e19b8dbe0d24628f6abb9c82144a881f92726a7bf16025b4b905b',
+VALUES (DEFAULT, 'bri@gmail.com', '4c5f32a0f26e19b8dbe0d24628f6abb9c82144a881f92726a7bf16025b4b905b',
         'e1LNFkbsqhTYUCVeQqUJ8myQP12QdIKbezFS17yztm7CvfjdiP9RD2SoZUgdgwSN'),
-  (2, 'kat@gmail.com', '63d533598e6a64ef5d7412177403d683f2d97ce3bd48dc27d9be764700e90611',
+  (DEFAULT, 'kat@gmail.com', '63d533598e6a64ef5d7412177403d683f2d97ce3bd48dc27d9be764700e90611',
    'NkXW96mpnH6PvIUFyGp2bqmJVUa4KsuITRXGPOnd0W4BrouQuFlBmcYgKdcm2qGc'),
-  (3, 'matt@gmail.com', '3fd6353619c0c1a1c2adac0a9a62653654fb249be16b5a1ba01d350ef0dbbf13',
+  (DEFAULT, 'matt@gmail.com', '3fd6353619c0c1a1c2adac0a9a62653654fb249be16b5a1ba01d350ef0dbbf13',
    'XdB5YOA2qHCeUcpgzorgp6elRIo3slPykipMUyEebFQwowQ3bxwfAQ3wIxEJy9KL');
 
 INSERT INTO public.language (
   languageid, language)
-VALUES (1, 'java'),
-  (2, 'javascript'),
-  (3, 'php');
+VALUES (DEFAULT, 'java'),
+  (DEFAULT, 'javascript'),
+  (DEFAULT, 'php');
 
 INSERT INTO public.tips (
   tipsid, accountid, languageid, description, rating)
 VALUES
-  (1, 1, 1,
-   'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-   3),
-  (2, 2, 1, 'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.', 6),
-  (3, 3, 1, 'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.', 75),
-  (4, 1, 1, 'Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.', -34),
-  (5, 1, 2, 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam.', 2),
-  (6, 2, 2, 'Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. ', 76),
-  (7, 3, 2, 'Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit.', -4),
-  (8, 1, 2, 'Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut.', 0),
-  (9, 1, 3, 'Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur.', 8),
-  (10, 2, 3, 'At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati.', -3),
-  (11, 3, 3, 'Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus.', 34),
-  (12, 1, 3,
-   'Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae.',
-   -21),
-  (13, 1, 3,
-   'Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat.',
-   3);
-
+  (DEFAULT, 1, 1, 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',3),
+  (DEFAULT, 2, 1, 'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.', 6),
+  (DEFAULT, 3, 1, 'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.', 75),
+  (DEFAULT, 1, 1, 'Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.', -34),
+  (DEFAULT, 1, 2, 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam.', 2),
+  (DEFAULT, 2, 2, 'Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. ', 76),
+  (DEFAULT, 3, 2, 'Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit.', -4),
+  (DEFAULT, 1, 2, 'Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut.', 0),
+  (DEFAULT, 1, 3, 'Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur.', 8),
+  (DEFAULT, 2, 3, 'At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati.', -3),
+  (DEFAULT, 3, 3, 'Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus.', 34),
+  (DEFAULT, 1, 3,'Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae.',-21),
+  (DEFAULT, 1, 3,'Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat.', 3);
 INSERT INTO public.userratings (
   accountid, tipsid)
 VALUES (1, 1),
@@ -85,4 +78,3 @@ VALUES (1, 1),
   (2, 10),
   (3, 1),
   (3, 2);
-	
